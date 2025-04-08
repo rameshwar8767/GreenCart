194cb2c7ec38b5790c6438d17eef16dd219092f5
rameshwar mane <manerameshwar909@gmail.com>
2025-04-09 00:49:39 +0530

    feat: Add ProductCategory page to display products by category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
 function App() {
  const isSellerPath = useLocation().pathname.includes('seller')
 const {showUserLogin} = useAppContext()
@@ -20,6 +21,7 @@ const {showUserLogin} = useAppContext()
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/products" element={<AllProducts/>} />
+          <Route path="/products/:category" element={<ProductCategory/>} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
